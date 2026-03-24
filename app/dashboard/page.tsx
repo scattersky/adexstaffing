779d38e6e503dc0650e334790f4168ce13573ee6
scattersky <development@scattersky.com>
2026-03-24 10:33:56 -0400

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,20 +2,32 @@
 
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
 
 export default function DashboardRouter() {
   const { role } = useAuth();
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (!role) {
-      router.push("/login");
       return;
     };
 
-    router.push(`/dashboard/${role}`);
+    if(role === 'admin' || role === 'recruiter' || role === 'candidate'){
+      router.push(`/dashboard/${role}`);
+    } else {
+      router.push('/login');
+    }
+
   }, [role]);
 
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="w-12 h-12 border-4 border-gray-300 border-t-red-700 rounded-full animate-spin"></div>
+      </div>
+    );
+  }
   return null;
 }
